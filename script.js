e89be314688a722858779a42d21c62b923f08f44
Ak47elite <es18btech11001@iith.ac.in>
2020-08-13 00:28:26 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 (function(window){
-    var names = ['Nicolai','Magisk','Gla1ve','JS','Javascript','Dupreeh'];
-
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for(var i=0; i<names.length; i++){
 
         var firstletter = (names[i].charAt(0)).toLowerCase();
